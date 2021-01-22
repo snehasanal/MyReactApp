7883827e8d916a4ec9dae77fa876cfa05a793349
snehasanal <snehaajitha24@gmail.com>
2021-01-22 13:55:00 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
-import TodoApp from "./TodoApp";
+import About from "./component/About/About";
+import Header from "./component/Header/Header";
+import TodoApp from "./component/TodoApp/TodoApp";
+import {BrowserRouter as Router,Route} from 'react-router-dom';
+
 class App extends Component {
   render() {
-    return <TodoApp />;
+    return (
+      <>
+      <Router>
+        <Header/>
+        <Route path="/" exact component={TodoApp}/>
+        <Route path="/about" component={About}/>
+      </Router>
+      </>
+    );
   }
 }
 
